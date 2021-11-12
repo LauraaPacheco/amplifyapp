3b3e9547b8f11a14fa556836beccb3339000deb4
LauraaPacheco <73205826+LauraaPacheco@users.noreply.github.com>
2021-11-11 19:20:49 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+importar  logo  desde  './logo.svg' ;
+importar  './App.css' ;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-  );
+función  App ( )  {
+  retorno  (
+    < div  className = "Aplicación" >
+      < header  className = "App-header" >
+        < img  src = { logo }  className = "App-logo"  alt = "logo"  / >
+        < p >
+          Edite < código > src / App.js < / código > y guárdelo para volver a cargarlo.
+        < / p >
+        < a
+          className = "App-link"
+          href = "https://reactjs.org"
+          target = "_blank"
+          rel = "noopener noreferrer"
+        >
+          Aprenda Reaccionar
+        < / a >
+      < / encabezado >
+    < / div >
+  ) ;
 }
 
-export default App;
+exportar la  aplicación predeterminada  ;
